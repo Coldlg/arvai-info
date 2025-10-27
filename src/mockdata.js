25faa2c0ec53a9ba4c292f2af2a69f9fbaaa4ea0
Coldglan <coldglan@Colds-MacBook-Pro.local>
2025-10-27 17:08:09 +0800

    version 1.0

---
+++ src/mockdata.js
@@ -0,0 +1,29 @@
+export const lotteries = [
+  {
+    id: 1,
+    title: "2024",
+    description: "",
+    ticketsAvailable: 1250,
+    maximumTickets: 5000,
+    price: 50000,
+    drawDate: "2024-12-15",
+  },
+  {
+    id: 2,
+    title: "2025",
+    description: "",
+    ticketsAvailable: 980,
+    maximumTickets: 3000,
+    price: 75000,
+    drawDate: "2025-01-20",
+  },
+  {
+    id: 3,
+    title: "2026",
+    description: "",
+    ticketsAvailable: 2100,
+    maximumTickets: 10000,
+    price: 60000,
+    drawDate: "2024-12-30",
+  },
+];
